cc4a5d0084bfa024becf1001709bf04f1e09dca3
suryavanshi18 <76112327+suryavanshi18@users.noreply.github.com>
2023-04-04 11:02:22 -0700

    Add files via upload

---
+++ Google/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Google.Models;
+using System.IO;
+using System.Web;
+namespace Google.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [HttpGet]
+        public ActionResult GetGoogleDriveFiles()
+        {
+            return View(GoogleDriveFilesRepository.GetDriveFiles());
+        }
+
+        [HttpPost]
+        public ActionResult DeleteFile(GoogleDriveFiles file)
+        {
+            GoogleDriveFilesRepository.DeleteFile(file);
+            return RedirectToAction("GetGoogleDriveFiles");
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            GoogleDriveFilesRepository.FileUpload(file);
+            return RedirectToAction("GetGoogleDriveFiles");
+        }
+
+        public void DownloadFile(string id)
+        {
+            string FilePath = GoogleDriveFilesRepository.DownloadGoogleFile(id);
+
+
+            Response.ContentType = "application/zip";
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(FilePath));
+            Response.WriteFile(System.Web.HttpContext.Current.Server.MapPath("~/GoogleDriveFiles/" + Path.GetFileName(FilePath)));
+            Response.End();
+            Response.Flush();
+        }
+    }
+}
